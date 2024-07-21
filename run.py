0e3016b3c6ea7f4bcc9afb15888f4b1842219219
fenasti zxc <td9z0vyv@students.codeinstitute.net>
2024-07-21 23:32:39 +0000

    separate function to get the data of the players

--- run.py
+++ run.py
@@ -4,6 +4,35 @@ class Player:
         self.name = name
         self.life_points = life_points
 
+def get_players_data():
+    print("Welcome to the Magic: The Gathering life points counter!")
+    print("Instructions:")
+    print("1. Choose game mode, Standard or Comander.")
+    print("2. Define each player names.")
+    print("3. Roll a dice to find out who starts.")
+
+    life_points = 0
+    game_mode = input("Enter game mode:").lower()
+    while if game_mode == "standard":
+            life_points = 20
+            break
+        elif game_mode == "comander":
+            life_points = 40
+            break
+        else:
+            print("Invalid mode, choose 'Standard' or 'Comander'.")
+
+
+
+    player1_name = input("Enter the name of Player 1: ")
+    player2_name = input("Enter the name of Player 2: ")
+    
+    player1 = Player(player1_name, game_mode)
+    player2 = Player(player2_name, game_mode)
+
+    players = [player1, player2]
+    
+
 def adjust_life():
     pass
 
@@ -12,35 +41,7 @@ def roll_dice():
 
 def main():
 
-    run_game = True
-
-    while run_game:
-        print("Welcome to the Magic: The Gathering life points counter!")
-        print("Instructions:")
-        print("1. Choose game mode, Standard or Comander.")
-        print("2. Define each player names.")
-        print("3. Roll a dice to find out who starts.")
-
-
-        game_mode = input("Enter game mode:").lower()
-        if game_mode == "standard":
-            life_points = 20
-            return life_points
-        elif game_mode == "comander":
-            life_points = 40
-            return life_points
-        else:
-            print("Invalid mode, choose 'Standard' or 'Comander'.")
-            
-
-
-        player1_name = input("Enter the name of Player 1: ")
-        player2_name = input("Enter the name of Player 2: ")
-        
-        player1 = Player(player1_name, game_mode)
-        player2 = Player(player2_name, game_mode)
-
-        players = [player1, player2]
+    
 
 
 
