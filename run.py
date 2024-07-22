b0c67810ef4ca9a1ef3e26eac46d020ddc9fb87d
fenasti zxc <td9z0vyv@students.codeinstitute.net>
2024-07-22 14:21:28 +0000

    clean and debug the variables scope to properly scope the player class

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ def get_players_data():
 
     return player1, player2
     
-def adjust_life(p1, p2):
+def adjust_life(player1, player2):
     while True:
         get_new_score = input("\nEnter action (e.g., 'Player1 -3' to subtract 3 life points from Player1 or 'exit' to quit): \n")
         if get_new_score.lower() == "exit":
@@ -43,12 +43,18 @@ def adjust_life(p1, p2):
             print("Invalid input. Please enter the action in the correct format.")
             continue
 
-        for player in players:
-            if player.name.lower() == name.lower():
-                player.adjust_life(change)
-                break
+        if name.lower() == player1.name.lower():
+            player1.adjust_life(new_score)
+        elif name.lower() == player2.name.lower():
+            player2.adjust_life(new_score)
         else:
             print(f"No player found with the name {name}.")
+            continue
+
+        print("\nUpdated Life Points:")
+        print(player1.life_points)
+        print(player2.life_points)
+
             
 
 
@@ -69,16 +75,14 @@ def main():
     print(player1.name, player1.life_points)
     print(player2.name, player2.life_points)
 
-    p1score = player1.life_points
-    p2score = player2.life_points
-    p1name = player1.name
-    p2name = player2.name
+   # p1score = player1.life_points
+   # p2score = player2.life_points
+   # p1name = player1.name
+   # p2name = player2.name
 
-    player_one = [p1name, p1score]
-    player_two = [p2name, p2score]
-
-    print(player_one, player_two)
-    new_score = adjust_life(player_one, player_two)
+   # player_one = [p1name, p1score]
+   # player_two = [p2name, p2score]
+    adjust_life(player1, player2)
 
 
 main()
