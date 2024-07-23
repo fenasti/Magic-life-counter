9608ece59d4f6c8fa9eba0596d6e10a79addb4fc
fenasti zxc <td9z0vyv@students.codeinstitute.net>
2024-07-23 00:10:29 +0000

    clean whitespaces and indentation for validation

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Player:
     """Define the name and the life points amount of each player"""
     def __init__(self, name, life_points):
@@ -8,32 +9,32 @@ class Player:
 
     def adjust_life(self, amount):
         self.life_points += amount
-        
+
+
 def print_ascii_art():
     art = """
-         .AMMMMMMMMMMA.          
-       .AV. :::.:.:.::MA.        
-      A' :..        : .:`A       
-     A'..              . `A.     
-    A' :.    :::::::::  : :`A    
-    M  .    :::.:.:.:::  . .M    
-    M  :   ::.:.....::.:   .M    
-    V : :.::.:........:.:  :V    
-   A  A:    ..:...:...:.   A A   
-  .V  MA:.....:M.::.::. .:AM.M   
- A'  .VMMMMMMMMM:.:AMMMMMMMV: A  
-:M .  .`VMMMMMMV.:A `VMMMMV .:M: 
- V.:.  ..`VMMMV.:AM..`VMV' .: V  
-  V.  .:. .....:AMMA. . .:. .V   
-   VMM...: ...:.MMMM.: .: MMV    
-       `VM: . ..M.:M..:::M'      
-         `M::. .:.... .::M       
-          M:.  :. .... ..M       
- VK       V:  M:. M. :M .V       
+         .AMMMMMMMMMMA.
+       .AV. :::.:.:.::MA.
+      A' :..        : .:`A
+     A'..              . `A.
+    A' :.    :::::::::  : :`A
+    M  .    :::.:.:.:::  . .M
+    M  :   ::.:.....::.:   .M
+    V : :.::.:........:.:  :V
+   A  A:    ..:...:...:.   A A
+  .V  MA:.....:M.::.::. .:AM.M
+ A'  .VMMMMMMMMM:.:AMMMMMMMV: A
+:M .  .`VMMMMMMV.:A `VMMMMV .:M:
+ V.:.  ..`VMMMV.:AM..`VMV' .: V
+  V.  .:. .....:AMMA. . .:. .V
+   VMM...: ...:.MMMM.: .: MMV
+       `VM: . ..M.:M..:::M'
+         `M::. .:.... .::M
+          M:.  :. .... ..M
+ VK       V:  M:. M. :M .V
           `V.:M.. M. :M.V'
     """
     print(art)
-
 
 
 def get_players_data():
@@ -45,7 +46,8 @@ def get_players_data():
     """
     def get_initial_life_points():
         while True:
-            game_mode = input("Enter game mode (Standard or Commander):\n").lower()
+            game_mode = input("""
+Enter game mode (Standard or Commander):\n""").lower()
             print()
             if game_mode == "standard":
                 return 20
@@ -53,22 +55,25 @@ def get_players_data():
                 return 40
             else:
                 print("Invalid mode, choose 'Standard' or 'Commander'.")
-                
 
     game_mode = get_initial_life_points()
 
     player1_name = input("Enter the name of Player 1:\n")
     print()
     player2_name = input("Enter the name of Player 2:\n")
-    
+
     player1 = Player(player1_name, game_mode)
     player2 = Player(player2_name, game_mode)
 
     return player1, player2
-    
+
+
 def calculate_life(player1, player2):
     while True:
-        get_new_score = input("\nEnter action (e.g., 'Player1 -3' to subtract 3 life points from Player1 or 'exit' to quit):\n")
+        get_new_score = input("""
+Enter action (e.g., 'Player1 -3' to subtract 3 
+life points from Player1 or 'exit' to quit):
+""")
         if get_new_score.lower() == "exit":
             break
 
@@ -76,7 +81,9 @@ def calculate_life(player1, player2):
             name, new_score = get_new_score.split()
             new_score = int(new_score)
         except ValueError:
-            print("Invalid input. Please enter the action in the correct format.")
+            print("""
+Invalid input. Please enter the action in the correct format.
+""")
             continue
 
         if name.lower() == player1.name.lower():
@@ -97,6 +104,7 @@ def calculate_life(player1, player2):
         print(f"{player1.name} is {player1.life_points} Lp")
         print(f"{player2.name} is {player2.life_points} Lp")
 
+
 def roll_dice(name1, name2):
     input("Press Enter to roll the dice and determine who starts...\n")
     dice1 = random.randint(1, 6)
@@ -107,7 +115,7 @@ def roll_dice(name1, name2):
         dice1 = random.randint(1, 6)
         dice2 = random.randint(1, 6)
         print(f"New rolls... {name1} rolled {dice1}, {name2} rolled {dice2}")
-            
+
 
 def main():
     print("Welcome to the Magic: The Gathering life points counter!")
@@ -128,5 +136,6 @@ def main():
     print()
     roll_dice(player1.name, player2.name)
     calculate_life(player1, player2)
+
 
 main()
